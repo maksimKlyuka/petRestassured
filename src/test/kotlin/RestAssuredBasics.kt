48106e3487478142745ce99390eb1c82f8efdb98
Максим <maksim@MacBook-Pro-Maksim.local>
2025-10-16 21:22:50 +0300

    добавил создание второго пользователя, поста и коммента

--- src/test/kotlin/RestAssuredBasics.kt
+++ src/test/kotlin/RestAssuredBasics.kt
@@ -17,15 +17,16 @@ class UserStoryTest {
         RestAssured.baseURI = "https://dummyapi.io/data/v1"
     }
 
-    private var idUser1 = "68efd7fa4229c5811d090463"
+    private var idUser1 = "68f1275efc305325fda8eb12"
     private var idUser2 = ""
     private var idPost = ""
     private var idComment = ""
-    private var email = "sivanov26@gmail.com"
-    private var timeUserCreate = ""
-    private var timeUserUpdate = ""
+    private var email = "sivanov28@gmail.com"
+    private var emailU2 = "mivanova28@gmail.com"
+    private var timeUserCreate = "" // общая
+    private var timeUserUpdate = "" // общая
     private var timePostCreate = ""
-    private var timePostUpdate = ""
+    private var timeCommentCreate = ""
 
     @Test
     @Order(1)
@@ -129,7 +130,6 @@ class UserStoryTest {
                 }
             }
         """.trimIndent()
-        timeUserUpdate = java.time.Instant.now().toString().substring(0, 16)
         val response = RestAssured.given()
             .contentType("application/json")
             .body(requestBody)
@@ -145,7 +145,7 @@ class UserStoryTest {
             .statusCode(200)
             .extract()
             .response()
-
+        timeUserUpdate = java.time.Instant.now().toString().substring(0, 16)
         SoftAssertions().apply {
             assertThat(response.header("Content-Type")).isEqualTo("application/json; charset=utf-8")
 
@@ -214,7 +214,7 @@ class UserStoryTest {
             "owner": "$idUser1"
             }
         """.trimIndent()
-        timePostCreate = java.time.Instant.now().toString().substring(0, 16)
+
         val response = RestAssured.given()
             .contentType("application/json")
             .body(requestBody)
@@ -232,6 +232,7 @@ class UserStoryTest {
             .response()
 
         idPost = response.path("id")
+        timePostCreate = java.time.Instant.now().toString().substring(0, 16)
 
         SoftAssertions().apply {
             assertThat(response.header("Content-Type")).isEqualTo("application/json; charset=utf-8")
@@ -251,6 +252,129 @@ class UserStoryTest {
 
     @Test
     @Order(6)
+    fun checkCreatePostUser1() {
+        val response = RestAssured.given()
+            .contentType("application/json")
+            .header("Content-Type", "application/json")
+            .header("app-id", "64ba6e63c4e32672bf5e6891") // спрячу потом
+            .log().all()
+
+            .`when`()
+            .get("/post/$idPost")
+
+            .then()
+            .log().all()
+            .statusCode(200)
+            .extract()
+            .response()
+
+        SoftAssertions().apply {
+            assertThat(response.header("Content-Type")).isEqualTo("application/json; charset=utf-8")
+
+            assertThat(response.path<String>("id")).isNotNull()
+            assertThat(response.path<Int>("likes")).isEqualTo(0)
+            assertThat(response.path<List<String>>("tags")).containsExactly("#AXE", "#Реклама")
+            assertThat(response.path<String>("text")).isEqualTo("Я сегодня бегал")
+            assertThat(response.path<String>("publishDate")).startsWith(timePostCreate)
+            assertThat(response.path<String>("updatedDate")).startsWith(timePostCreate)
+            assertThat(response.path<String>("owner.id")).isEqualTo(idUser1)
+            assertThat(response.path<String>("owner.title")).isEqualTo("mr")
+            assertThat(response.path<String>("owner.firstName")).isEqualTo("Александр")
+            assertThat(response.path<String>("owner.lastName")).isEqualTo("Иванов-Прокофьев")
+        }.assertAll()
+    }
+
+    @Test
+    @Order(7)
+    fun createUser2() {
+        val requestBody = """
+            {
+            "title": "ms",
+            "firstName": "Маша",
+            "lastName": "Иванова",
+            "email": "$emailU2",
+            "dateOfBirth": "05/06/1994",
+            "phone": "+79998765566"
+            }
+        """.trimIndent()
+        timeUserCreate = java.time.Instant.now().toString().substring(0, 16)
+
+        val response = RestAssured.given()
+            .contentType("application/json")
+            .body(requestBody)
+            .header("Content-Type", "application/json")
+            .header("app-id", "64ba6e63c4e32672bf5e6891") // спрячу потом
+            .log().all()
+
+            .`when`() // Выполнение запроса
+            .post("/user/create")
+
+            .then()
+            .log().all()
+            .statusCode(200)
+            .extract()
+            .response()
+
+        idUser2 = response.path<String>("id")
+
+            SoftAssertions().apply {
+                assertThat(response.header("Content-Type")).isEqualTo("application/json; charset=utf-8")
+
+                assertThat(response.path<String>("id")).isNotNull()
+                assertThat(response.path<String>("title")).isEqualTo("ms")
+                assertThat(response.path<String>("firstName")).isEqualTo("Маша")
+                assertThat(response.path<String>("lastName")).isEqualTo("Иванова")
+                assertThat(response.path<String>("email")).isEqualTo(emailU2)
+                assertThat(response.path<String>("dateOfBirth")).isEqualTo("1994-05-06T00:00:00.000Z")
+                assertThat(response.path<String>("phone")).isEqualTo("+79998765566")
+                assertThat(response.path<String>("registerDate")).startsWith(timeUserCreate)
+                assertThat(response.path<String>("updatedDate")).startsWith(timeUserCreate)
+            }.assertAll()
+    }
+
+    @Test
+    @Order(8)
+    fun createComment() {
+        val requestBody = """
+            {
+            "message": "Но футболка сухая и совсем не пахнет",
+            "owner":"$idUser2", 
+            "post": "$idPost"
+            }
+        """.trimIndent()
+        val response = RestAssured.given()
+            .contentType("application/json")
+            .body(requestBody)
+            .header("Content-Type", "application/json")
+            .header("app-id", "64ba6e63c4e32672bf5e6891") // спрячу потом
+            .log().all()
+
+            .`when`()
+            .post("/comment/create")
+
+            .then()
+            .log().all()
+            .statusCode(200)
+            .extract()
+            .response()
+
+        timeCommentCreate = java.time.Instant.now().toString().substring(0, 16)
+        idComment = response.path("id")
+
+        SoftAssertions().apply {
+            assertThat(response.header("Content-Type")).isEqualTo("application/json; charset=utf-8")
+
+            assertThat(response.path<String>("message")).isEqualTo("Но футболка сухая и совсем не пахнет")
+            assertThat(response.path<String>("owner.id")).isEqualTo(idUser2)
+            assertThat(response.path<String>("owner.firstName")).isEqualTo("Маша")
+            assertThat(response.path<String>("owner.lastName")).isEqualTo("Иванова")
+            assertThat(response.path<String>("post")).isEqualTo(idPost)
+            assertThat(response.path<String>("publishDate")).startsWith(timeCommentCreate)
+        }
+    }
+
+    @Test
+    @Order(9)
     fun deleteUser1() {
         RestAssured.given()
             .contentType("application/json")
@@ -264,6 +388,24 @@ class UserStoryTest {
             .then() // Валидация ответа
             .statusCode(200)
             .body("id", Matchers.equalTo(idUser1))
+            .log().all()
+    }
+
+    @Test
+    @Order(10)
+    fun deleteUser2() {
+        RestAssured.given()
+            .contentType("application/json")
+            .header("Content-Type", "application/json")
+            .header("app-id", "64ba6e63c4e32672bf5e6891")
+            .log().all()
+
+            .`when`()
+            .delete("/user/$idUser2")
+
+            .then() // Валидация ответа
+            .statusCode(200)
+            .body("id", Matchers.equalTo(idUser2))
             .log().all()
     }
 }
